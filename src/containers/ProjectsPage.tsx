5a6a57810b5da8698164d352e3807ceb27709b32
Max Zhang <max.keervision@gmail.com>
2021-06-05 09:36:51 -0700

    declare router

--- src/containers/ProjectsPage.tsx
+++ src/containers/ProjectsPage.tsx
@@ -1,11 +1,11 @@
-import { useContext } from 'react';
-import styled, { ThemeContext } from 'styled-components';
+// import { useContext } from 'react';
+// import styled, { ThemeContext } from 'styled-components';
 import { typography, containers } from '../styles';
 import { SideNav } from '../components/SideNav';
 
 export const ProjectsPage = () => {
-    const themeContext:any = useContext(ThemeContext);
-    const isDark = themeContext.id === 'dark';
+    // const themeContext:any = useContext(ThemeContext);
+    // const isDark = themeContext.id === 'dark';
     
     return(
             <containers.SiteContainer>
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,3 @@
 declare module '*.pdf';
 declare module 'styled-components';
+declare module 'react-router-dom';
